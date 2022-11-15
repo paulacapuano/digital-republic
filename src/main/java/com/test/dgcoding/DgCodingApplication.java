9d36495a8882115f8c958d88962680a117b8ec35
paulacapuano <paula.macedo@bandtec.com.br>
2022-11-15 15:47:46 -0300

    initial config

---
+++ src/main/java/com/test/dgcoding/DgCodingApplication.java
@@ -0,0 +1,13 @@
+package com.test.dgcoding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DgCodingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DgCodingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/test/dgcoding/DgCodingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.dgcoding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DgCodingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
